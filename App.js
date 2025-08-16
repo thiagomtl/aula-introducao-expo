24a0477f8cc22b04582fda61e1323ef69c72b13e
Thiago de Melo Torres Lopes <thiagodemelo2001@hotmail.com>
2025-08-15 22:45:40 -0300

    Começando Exemplo 01

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Exemplo01 from './src/exemplos/ex01';
+
 export default function App() {
   return (
     <View style={styles.container}>
---
+++ src/exemplos/ex01/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+
+export default function Exemplo01() {
+    return(
+        <View>
+            <Text>Exemplo 1</Text>
+            <Text>Introdução React Native com Expo</Text>
+        </View>
+    );
+}
