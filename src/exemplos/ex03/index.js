5173382c24519e5996193c48e92bab58df981399
Thiago de Melo Torres Lopes <thiagodemelo2001@hotmail.com>
2025-09-12 20:39:22 -0300

    Aula 12/09

--- src/exemplos/ex03/index.js
+++ src/exemplos/ex03/index.js
@@ -1,4 +1,4 @@
-import { useState} from 'react';
+import { useState } from 'react';
 
 import { View, Text, Button, TouchableOpacity } from 'react-native';
 
@@ -10,27 +10,52 @@ export default function Exemplo03() {
 
     function handleIncrementar() {
         setNumero(numero + 1);
-       // console.log(numero);
     }
-    
+
+    function handleDecrementar() {
+        setNumero(numero - 1);
+    }
+
+    function handleLimpar() {
+        setNumero(0);
+    }
+
     return (
         <View style={styles.container}>
-            <Text style={styles.titulo}>Exemplo 3</Text>
+            <Text style={styles.titulo}>Atividade 3</Text>
             <Button
                 onPress={() => { alert('Alguém tocou no botão!') }}
                 title='Alerta'
                 color='#777'
                 accessibilityLabel='Botão de alerta'
             />
+        
 
-            <Text style={styles.txt}>{numero}</Text>
+            <View style={styles.containerbotao}>
+                <TouchableOpacity
+                    style={styles.botao}
+                    onPress={() => handleIncrementar()}
+                >
+                    <Text style={styles.txtBotao}>+</Text>
+                </TouchableOpacity>
+                <Text style={styles.txt}>{numero}</Text>
 
+                <TouchableOpacity
+                    style={styles.botao}
+                    onPress={() => handleDecrementar()}
+                >
+                    <Text style={styles.txtBotao}>-</Text>
+                </TouchableOpacity>
+
+                
+            </View>
             <TouchableOpacity
-                style={styles.botao}
-                onPress={() => handleIncrementar()}
-            >
-                <Text style={styles.txtBotao}>Incrementar número</Text>
-            </TouchableOpacity>
+                    style={styles.botao2}
+                    onPress={() => handleLimpar()}
+                >
+                    <Text style={styles.txtBotao}>Limpar</Text>
+                </TouchableOpacity>
         </View>
+        
     );
 }
--- src/exemplos/ex03/styles.js
+++ src/exemplos/ex03/styles.js
@@ -4,27 +4,46 @@ import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 const styles = StyleSheet.create(
     {
         container: {
-            flex:1,
-            backgroundColor: 'deepskyblue',
-            alignItems:'center',
-            justifyContent:'space-evenly',
+            flex: 1,
+            backgroundColor: 'white',
+            alignItems: 'center',
+            justifyContent: 'center',
+            border: 'black',
+            // borderWidth: 10,
+            gap: RFPercentage(2),
+        },
+        containerbotao: {
+            backgroundColor: 'white',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'row',
+            gap: RFPercentage(5),
         },
         titulo: {
-            fontSize:RFPercentage(5),
+            fontSize: RFPercentage(5),
             fontWeight: 'bold',
-            color:'#777',
+            color: '#777',
         },
-        txt:{
+        txt: {
             fontSize: RFPercentage(2.5),
+            color: 'black',
+            fontWeight: 'bold',
         },
-        botao:{
+        botao: {
             backgroundColor: '#777',
-            width: '60%',
+            width: '10%',
             borderRadius: RFPercentage(1),
             padding: RFPercentage(1),
             alignItems: 'center',
         },
-        txtBotao:{
+        botao2: {
+            backgroundColor: '#777',
+            width: '40%',
+            borderRadius: RFPercentage(1),
+            padding: RFPercentage(1),
+            alignItems: 'center',
+        },
+        txtBotao: {
             color: '#ff0',
             fontSize: RFPercentage(2),
             fontWeight: 'bold',
